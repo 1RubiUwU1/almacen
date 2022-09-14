d18af45fb02c6af7359da12c624665036cbb49f9
hola <110574193+1RubiUwU1@users.noreply.github.com>
2022-09-13 20:23:02 -0500

    Add files via upload

---
+++ AutoBean_Spam.lua
@@ -0,0 +1,20 @@
+--[Made By D_Raziq225]--
+--[Press C to toggle]--
+--[Press V to untoggle]--
+Raziq = true
+lolers = false
+
+local mouse = game.Players.LocalPlayer:GetMouse()
+mouse.KeyDown:connect(function(key)
+if key == "v" then 
+_G.Noslow = Raziq
+elseif key == "l" then
+_G.Noslow = lolers
+end
+end)
+
+while wait(0.5) do 
+if _G.Noslow == true then
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.EatSenzu:FireServer(true) 
+end 
+end
